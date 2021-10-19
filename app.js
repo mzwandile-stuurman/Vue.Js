6cc490f6a759d94482b3b0f5842f5a094a9a3e27
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2021-10-19 13:56:19 +0200

    vue section2

--- app.js
+++ app.js
@@ -6,11 +6,25 @@ const app = Vue.createApp({
       showBook: true,
       url: "https://www.thenetninja.co.uk",
       books: [
-          { title: 'name of wind', author: 'Patrick Rothfuss' },
-          { title: 'The way of Kings', author: 'brandon sanderson' },
-          { title: 'the final empire', author: 'brandoon sanderson' },
-
-      ]
+        {
+          title: "name of wind",
+          author: "Patrick Rothfuss",
+          img: "./assets/1.png",
+          isFav: true,
+        },
+        {
+          title: "The way of Kings",
+          author: "brandon sanderson",
+          img: "./assets/2.png",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandoon sanderson",
+          img: "./assets/3.png",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
@@ -19,16 +33,22 @@ const app = Vue.createApp({
     },
     handleEvent(e, data) {
       console.log(e, e.type);
-      if (data){
-          console.log(data)
+      if (data) {
+        console.log(data);
       }
     },
-    handleMouseMove(e){
-        this.x = e.offsetX
-        this.y = e.offsetY
-
-
-    }
+    handleMouseMove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    fiteredBooks() {
+      return this.books.filter((book) => book.isFav);
+    },
   },
 });
 app.mount("#app");
